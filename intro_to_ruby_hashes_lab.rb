e68efc255b0bc6e04c4df9d2bc0c60df9126803c
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-10 02:19:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
 	  :railroads => {
 	    :pieces => 4, 
 	    :names => {
-	      :reading_railroad => {"mortgage_valu" => '$100'},
+	      :reading_railroad => {'mortgage_valu' => '$100'},
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
